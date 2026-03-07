23955935ee14ed72ea8a4259fac0c651ca2c5db9
Manish-Dhane <manishdhane2004@gmail.com>
2026-03-08 00:08:09 +0530

    Fix seed data with geometry and owner reference

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
+const dns = require("dns");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 
 
--- init/index.js
+++ init/index.js
@@ -1,8 +1,14 @@
+if(process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const dns = require("dns");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const MONGO_URL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wonderlust";
 
 main()
   .then(() => {
@@ -16,11 +22,24 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+// const initDB = async () => {
+//   await Listing.deleteMany({});
+//   initData.data = initData.data.map((obj) =>({...obj, owner:"685c0479d532e20b48067d68",})); //assigning default owner to each data object
+//   await Listing.insertMany(initData.data);
+//   console.log("data was initialized");
+// };
+
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) =>({...obj, owner:"685c0479d532e20b48067d68",})); //assigning default owner to each data object
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+
+  const updatedListings = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68a09af20a1108de866b9a7a"
+  }));
+
+  await Listing.insertMany(updatedListings);
+
+  console.log("Database initialized");
 };
 
 initDB();
